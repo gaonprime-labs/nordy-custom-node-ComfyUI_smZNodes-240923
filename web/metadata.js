d49a4e7bb11ff2b5b2ec40fd0973020ddaaf221c
shpark <shpark@gaonprime.com>
2024-09-23 18:53:20 +0900

    Fix incorrect path for exif.js in metadata.js

--- web/metadata.js
+++ web/metadata.js
@@ -35,8 +35,8 @@ app.registerExtension({
         }
         const _exifPath = exifPath? exifPath.toLowerCase() : ""
         if (!exifPath || !_exifPath.includes('ComfyUI') || !_exifPath.includes('smz') || !_exifPath.includes('exif.js'))
-            exifPath = '/extensions/ComfyUI_smZNodes/js/exif.js'
-        exifPath = `${window.COMFY_API_URL}/extensions/nordy-custom-node-ComfyUI_smZNodes-240923/js/exif.js`
+            exifPath = '/extensions/ComfyUI_smZNodes/web/exif.js'
+        exifPath = `${window.COMFY_API_URL}/extensions/nordy-custom-node-ComfyUI_smZNodes-240923/web/exif.js`
         externalScript.src = exifPath;
         externalScript.onload = function(e) {
             _EXIF = EXIF
